abcbad0f38a92aa67c83e1ee7ed4373f0ecae586
EdwarF7 <edwar.fanoun@elev.ga.ntig.se>
2021-12-06 13:10:55 +0100

    Övning 1

--- Program.cs
+++ Program.cs
@@ -18,6 +18,11 @@ namespace Samlingsklasser
             Console.WriteLine("5. Tärningskast med SortedList (Övning 1).");
             Console.WriteLine("6. Queue-exempel.");
             Console.WriteLine("7. Stack-exempel.");
+            Console.WriteLine("8. Övning 2.");
+            Console.WriteLine("11. Övning 3.");
+            Console.WriteLine("12. Övning 4.");
+            Console.WriteLine("13. Övning 5.");
+
 
             //Läs in menyval
             Console.Write("Ange siffra för vad du vill göra: ");
@@ -46,6 +51,19 @@ namespace Samlingsklasser
                 case "7":
                     StackExempel();
                     break;
+                case "8":
+                    Övning2();
+                    break;
+                case "9":
+                    Övning3();
+                    break;
+                case "10":
+                    Övning4();
+                    break;
+                case "11":
+                    Övning5();
+                    break;
+
             }
 
             Console.ReadKey();
@@ -74,6 +92,32 @@ namespace Samlingsklasser
         static void DiceSortedList()
         {
             //Övning 1
+
+             //Skapa en SortedList med int som nyckel och int som värde
+            SortedList<int, int> resultat = new SortedList<int, int>();
+
+            //skapa ett Random objekt för att slumpa
+            Random random = new Random();
+
+            //Gör tusen upprepnigar
+            for (int i = 0; i < 1000; i++)
+            {
+                //Slumpa tal mellan 1 och 6
+                int tal = random.Next(1, 7);
+
+                //Lägg nyckel om denna inte redan finns
+                if (!resultat.ContainsKey(tal))
+                    resultat.Add(tal, 0);
+
+                //Öka förekomsten av tal
+                resultat[tal]++;
+            }
+
+            //Visa resultatet
+            foreach (KeyValuePair<int, int> kvp in resultat)
+            {
+                Console.WriteLine("Nyckel: {0} Värde: {1}", kvp.Key, kvp.Value);
+            }
 
         }
 
@@ -212,6 +256,44 @@ namespace Samlingsklasser
             {
                 Console.WriteLine("Värde: " + d);
             }
+
+        }
+
+        static void Övning2()
+        {
+            //Skapa en lista av double
+            List<double> Övning2lista = new List<double>();
+
+            for( int i=0; i<999999; i++ )
+            {
+                Console.WriteLine(" Mata in ett tal: ");
+                double input = Console.ReadLine();
+                Övning2lista.Add(input);
+            }
+
+            if (input == 0)
+            {
+                
+            }
+
+
+            Console.WriteLine("Medelvärdet: " + Övning2lista.Average().ToString("0.00"));
+
+
+        }
+
+        static void Övning3()
+        {
+
+        }
+
+        static void Övning4()
+        {
+
+        }
+
+        static void Övning5()
+        {
 
         }
     }
